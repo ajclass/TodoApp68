dc2d34dc48b7f4b9cb8f1998e842b90f20f72abf
Jack <jack@dev.it>
2025-04-02 13:44:10 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Weerachai Anotaipaiboon on 2/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
